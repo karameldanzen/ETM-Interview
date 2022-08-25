7d36c2da4a6e9fbc7da5fe6cf324e76bcfb2e66b
kstrayer97 <kstrayer.96@gmail.com>
2022-08-25 12:35:28 -0400

    Starter code

---
+++ ETM-Interview/ETM-Interview.cpp
@@ -0,0 +1,12 @@
+﻿// ETM-Interview.cpp : Defines the entry point for the application.
+//
+
+#include "ETM-Interview.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
